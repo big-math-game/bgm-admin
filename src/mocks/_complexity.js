1b25e3ed59a23c880e39ab7508d9d7a9409ee143
somakun <somabaka@gmail.com>
2020-12-24 22:28:40 +0300

    added mocks files

---
+++ src/mocks/_complexity.js
@@ -0,0 +1,16 @@
+const Complexity = [
+  {
+    id: 'easy',
+    name: 'Easy',
+  },
+  {
+    id: 'medium',
+    name: 'Medium',
+  },
+  {
+    id: 'hard',
+    name: 'Hard',
+  },
+]
+
+export default Complexity
---
+++ src/mocks/_topics.js
@@ -0,0 +1,6 @@
+const Topics = [
+  { id: 1, topic: 'sum' },
+  { id: 2, topic: 'sub' },
+];
+
+export default Topics;
