31db6ded2935843e9a5e031a2bbe4ba449c5358c
somakun <somabaka@gmail.com>
2020-12-25 13:28:36 +0300

    added value to mock

--- src/mocks/_complexity.js
+++ src/mocks/_complexity.js
@@ -1,14 +1,17 @@
 const Complexity = [
   {
     id: 'easy',
+    value: 0,
     name: 'Easy',
   },
   {
     id: 'medium',
+    value: 1,
     name: 'Medium',
   },
   {
     id: 'hard',
+    value: 2,
     name: 'Hard',
   },
 ]
