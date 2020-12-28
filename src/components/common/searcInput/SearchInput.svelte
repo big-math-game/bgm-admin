9714858ae3ee46abd874ecf6e893b35278158948
somakun <somabaka@gmail.com>
2020-12-28 17:49:38 +0300

    added search component

---
+++ src/components/common/searcInput/SearchInput.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  export let items = []
+  export let searchProperties = []
+  let searchTerm: string = ''
+  let selectedIndex = 0
+  let selectedProperty = ''
+  $: filteredList = items.filter((item) => item.login.indexOf(searchTerm) !== -1)
+</script>
+
+<article class="panel is-primary">
+  <p class="panel-heading">Select a player</p>
+  <p class="panel-tabs">
+    {#each searchProperties as property, i (i)}
+      <a
+        class:is-active="{i === selectedIndex}"
+        on:click="{() => {
+          selectedIndex = i
+          selectedProperty = property
+        }}">{property}</a>
+    {/each}
+  </p>
+  <div class="panel-block">
+    <p class="control has-icons-left">
+      <label for="search-input"></label>
+      <input id="search-input" class="input is-primary" type="text" placeholder="Search" bind:value="{searchTerm}" />
+      <span class="icon is-left"> <i class="fas fa-search" aria-hidden="true"></i> </span>
+    </p>
+  </div>
+  {#if searchTerm}
+    {#each filteredList as list, i (list.id)}
+      <a class="panel-block is-active">
+        <span class="panel-icon"> <i class="fas fa-user" aria-hidden="true"></i> </span>
+        {list.login + ' ' + list.email}
+      </a>
+    {/each}
+  {/if}
+</article>
