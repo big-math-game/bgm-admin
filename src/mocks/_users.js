5c6c37cece44621e427a833d1829048c930f9784
somakun <somabaka@gmail.com>
2020-12-28 17:48:52 +0300

    added user mock

---
+++ src/mocks/_users.js
@@ -0,0 +1,29 @@
+const users = [
+  {
+    id: 'cd6d3d18-b820-48e0-a443-4ca2acfbbe55',
+    email: 'user@user.ru',
+    login: 'Andrey'
+  },
+  {
+    id: 'cd6d3d18-b820-48e0-a443-as1d3sa58das',
+    email: 'user@user.ru',
+    login: 'Anton'
+  },
+  {
+    id: 'cd6d3d18-b820-48e0-a443-f1h3tf1h581fh',
+    email: 'user@user.ru',
+    login: 'Antony'
+  },
+  {
+    id: 'cd6d3d18-b820-48e0-a443-ada1a3dw1daw3',
+    email: 'user@user.ru',
+    login: 'Anastasia'
+  },
+  {
+    id: 'cd6d3d18-b820-48e0-a443-adadw15a3wd1a3',
+    email: 'user@user.ru',
+    login: 'Anon'
+  }
+]
+
+export default users
