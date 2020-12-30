a11a898fb19f1b304a4d53fb79a75e9cb6667954
somakun <somabaka@gmail.com>
2020-12-30 19:46:54 +0300

    added modal component

---
+++ src/components/common/modal/Modal.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let active: boolean = false
+  const close = () => {
+    active = false
+  }
+</script>
+
+<div class="{`modal ${active ? 'is-active' : ''}`}">
+  <div class="modal-background" on:click="{close}"></div>
+  <div class="modal-content">
+    <slot />
+  </div>
+  <button class="modal-close is-large" aria-label="close" on:click="{close}"></button>
+</div>
