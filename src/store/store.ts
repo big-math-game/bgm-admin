c0d01b3c2b155e1f31a224891682a09cc39c72af
somakun <somabaka@gmail.com>
2021-03-02 19:07:02 +0300

    rename theme to blank

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,5 +2,5 @@ import { writable } from 'svelte/store'
 
 export const templateList = writable([])
 export const themesList = writable([])
-export const theme = writable({})
+export const blank = writable({})
 export const task = writable({})
